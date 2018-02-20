bb1ec29f1a737b80aea87afa69b2c553725071b6
andry81 <andry@inbox.ru>
2018-02-20 19:24:59 +0000

    * new: spin sleep
    git-svn-id: svn+ssh://svn.code.sf.net/p/tacklelib/tacklelib/trunk@28 ffccfebd-399a-4e7c-9cd2-5fb927d1fecb

--- src/utility/algorithm.hpp
+++ src/utility/algorithm.hpp
@@ -5,8 +5,11 @@
 #include <utility/utility.hpp>
 #include <utility/static_assert.hpp>
 #include <utility/type_traits.hpp>
+#include <utility/math.hpp>
 
 #include <boost/type_traits/is_pod.hpp>
+
+#include <boost/chrono.hpp>
 
 #include <limits>
 #include <memory>
@@ -26,6 +29,8 @@ STATIC_ASSERT_GE(TACKLE_PP_MAX_UNROLLED_COPY_SIZE, TACKLE_PP_DEFAULT_UNROLLED_CO
 
 namespace utility
 {
+    using namespace boost::chrono;
+
     // for iterators debugging
     template<typename T>
     FORCE_INLINE bool is_singular_iterator(const T & it)
@@ -59,6 +64,81 @@ namespace utility
         else {
             for (size_t i = 0; i < size; i++) {
                 to[i] = from[i];
+            }
+        }
+    }
+
+    FORCE_INLINE void spin_sleep(uint64_t wait_nsec)
+    {
+        const auto begin_wait_time = high_resolution_clock::now();
+
+        while (true)
+        {
+            const auto next_wait_time = high_resolution_clock::now();
+
+            const auto spent_time_dur = next_wait_time - begin_wait_time;
+
+            const uint64_t spent_time_dur_nsec = spent_time_dur.count() >= 0 ? // workaround for negative values
+                duration_cast<nanoseconds>(spent_time_dur).count() : 0;
+
+            if (spent_time_dur_nsec >= wait_nsec) {
+                return;
+            }
+        }
+    }
+
+    template <typename Functor>
+    FORCE_INLINE void spin_sleep(uint64_t wait_nsec, Functor && spin_function)
+    {
+        const auto begin_wait_time = high_resolution_clock::now();
+
+        while (true)
+        {
+            const auto next_wait_time = high_resolution_clock::now();
+
+            const auto spent_time_dur = next_wait_time - begin_wait_time;
+
+            const uint64_t spent_time_dur_nsec = spent_time_dur.count() >= 0 ? // workaround for negative values
+                duration_cast<nanoseconds>(spent_time_dur).count() : 0;
+
+            if (spent_time_dur_nsec >= wait_nsec) {
+                return;
+            }
+
+            if (!spin_function()) {
+                return;
+            }
+        }
+    }
+
+    template <typename Functor>
+    FORCE_INLINE void spin_sleep(uint64_t wait_nsec, Functor && spin_function, uint64_t schedule_call_time_nsec)
+    {
+        const auto begin_wait_time = high_resolution_clock::now();
+
+        uint64_t schedule_time_next_index;
+        uint64_t schedule_time_prev_index = math::uint64_max;
+
+        while (true)
+        {
+            const auto next_wait_time = high_resolution_clock::now();
+
+            const auto spent_time_dur = next_wait_time - begin_wait_time;
+
+            const uint64_t spent_time_dur_nsec = spent_time_dur.count() >= 0 ? // workaround for negative values
+                duration_cast<nanoseconds>(spent_time_dur).count() : 0;
+
+            if (spent_time_dur_nsec >= wait_nsec) {
+                return;
+            }
+
+            schedule_time_next_index = spent_time_dur_nsec / schedule_call_time_nsec;
+
+            if (schedule_time_next_index != schedule_time_prev_index) {
+                if (!spin_function()) {
+                    return;
+                }
+                schedule_time_prev_index = schedule_time_next_index;
             }
         }
     }
