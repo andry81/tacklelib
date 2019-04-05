e6c6bc67196e251deb6b40529370e674a8ee6642
andry81 <andry@inbox.ru>
2019-04-05 11:14:05 +0000

    2019.04.05: * changed: copied latest `bash_entry` implementation directly into standalone scripts for admin maintaince purposes w/o `bash_entry` installation requirement
    git-svn-id: svn+ssh://svn.code.sf.net/p/tacklelib/tacklelib/trunk@123 ffccfebd-399a-4e7c-9cd2-5fb927d1fecb

--- _scripts/deploy/gen_links.sh
+++ _scripts/deploy/gen_links.sh
@@ -3,24 +3,111 @@
 # Script ONLY for execution.
 if [[ -n "$BASH" && (-z "$BASH_LINENO" || ${BASH_LINENO[0]} -eq 0) ]]; then 
 
-if [[ "$(type -t ScriptBaseInit)" != "function" ]]; then
-  function ScriptBaseInit
-  {
-    if [[ -n "$BASH_LINENO" ]] && (( ${BASH_LINENO[0]} > 0 )); then
-      ScriptFilePath="${BASH_SOURCE[0]//\\//}"
-    else
-      ScriptFilePath="${0//\\//}"
-    fi
-    if [[ "${ScriptFilePath:1:1}" == ":" ]]; then
-      ScriptFilePath="`/bin/readlink -f "/${ScriptFilePath/:/}"`"
-    else
-      ScriptFilePath="`/bin/readlink -f "$ScriptFilePath"`"
-    fi
+function ScriptBaseInit()
+{
+  if [[ -n "$BASH_LINENO" ]] && (( ${BASH_LINENO[${#BASH_LINENO[@]}-1]} > 0 )); then
+    local ScriptFilePath="${BASH_SOURCE[${#BASH_LINENO[@]}-1]//\\//}"
+  else
+    local ScriptFilePath="${0//\\//}"
+  fi
 
-    ScriptDirPath="${ScriptFilePath%[/]*}"
-    ScriptFileName="${ScriptFilePath##*[/]}"
-  }
-fi
+  GetAbsolutePathFromDirPath "$ScriptFilePath"
+  ScriptFilePath="${RETURN_VALUE}"
+
+  local ScriptDirPath="${ScriptFilePath%[/]*}"
+  local ScriptFileName="${ScriptFilePath##*[/]}"
+
+  return_local ScriptFilePath "${ScriptFilePath}"
+  return_local ScriptDirPath "${ScriptDirPath}"
+  return_local ScriptFileName "${ScriptFileName}"
+}
+
+# WORKAROUND:
+#   The `declare -g` has been introduced in the `bash-4.2-alpha`, so to make
+#   a global variable in an older version we have to replace the
+#   `declare -g` by a sequence of calls to `unset` and `eval`.
+#
+#   The `return_local` has used for both issues:
+#   1. To return a local variable.
+#   2. To replace the `declare -g`.
+#
+function return_local()
+{
+  unset $1 # must be local
+  eval "$1=\"\$2\""
+}
+
+function GetAbsolutePathFromDirPath()
+{
+  # drop return value
+  RETURN_VALUE="$1"
+
+  local DirPath="$1"
+  local RelativePath="$2"
+
+  # WORKAROUND:
+  #   Because some versions of readlink can not handle windows native absolute
+  #   paths correctly, then always try to convert directory path to a backend
+  #   path before the readlink in case if the path has specific native path
+  #   characters.
+  if [[ "${DirPath:1:1}" == ":" ]]; then
+    ConvertNativePathToBackend "$DirPath"
+    DirPath="$RETURN_VALUE"
+  fi
+
+  if [[ -n "$DirPath" && -x "/bin/readlink" ]]; then
+    if [[ "${RelativePath:0:1}" != '/' ]]; then
+      RETURN_VALUE="`/bin/readlink -m "$DirPath${RelativePath:+/}$RelativePath"`"
+    else
+      RETURN_VALUE="`/bin/readlink -m "$RelativePath"`"
+    fi
+  fi
+
+  return 0
+}
+
+function ConvertNativePathToBackend()
+{
+  # drop return value
+  RETURN_VALUE="$1"
+
+  # Convert all back slashes to slashes.
+  local PathToConvert="${1//\\//}"
+
+  [[ -n "$PathToConvert" ]] || return 1
+
+  # workaround for the bash 3.1.0 bug for the expression "${arg:X:Y}",
+  # where "Y == 0" or "Y + X >= ${#arg}"
+  local PathToConvertLen=${#PathToConvert}
+  local PathPrefixes=('' '')
+  local PathSuffix=""
+  if (( PathToConvertLen > 0 )); then
+    PathPrefixes[0]="${PathToConvert:0:1}"
+  fi
+  if (( PathToConvertLen > 1 )); then
+    PathPrefixes[1]="${PathToConvert:1:1}"
+  fi
+  if (( PathToConvertLen >= 3 )); then
+    PathSuffix="${PathToConvert:2}"
+  fi
+  PathSuffix="${PathSuffix%/}"
+
+  # Convert path drive prefix too.
+  if [[ "${PathPrefixes[0]}" != '/' && "${PathPrefixes[0]}" != '.' && "${PathPrefixes[1]}" == ':' ]]; then
+    case "$OSTYPE" in
+      "cygwin") PathToConvert="/cygdrive/${PathPrefixes[0]}$PathSuffix" ;;
+      *)
+        PathToConvert="/${PathPrefixes[0]}$PathSuffix"
+        # add slash to the end of path in case of drive only path
+        (( ! ${#PathSuffix} )) && PathToConvert="$PathToConvert/"
+      ;;
+    esac
+  fi
+
+  RETURN_VALUE="$PathToConvert"
+
+  return 0
+}
 
 ScriptBaseInit "$@"
 
