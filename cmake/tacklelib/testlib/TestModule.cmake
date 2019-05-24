a4bc196099c7d40558dbd19eee7357c86646ff63
andry81 <andry@inbox.ru>
2019-05-24 22:50:04 +0000

    2019.05.25: * fixed: cmake: `--path_match_filter` usage in `TestLib.cmake` * fixed: cmake: missed variable expansion in `TestLib.cmake` and `TestModule.cmake`
    git-svn-id: svn+ssh://svn.code.sf.net/p/tacklelib/tacklelib/trunk@157 ffccfebd-399a-4e7c-9cd2-5fb927d1fecb

--- cmake/tacklelib/testlib/TestModule.cmake
+++ cmake/tacklelib/testlib/TestModule.cmake
@@ -32,13 +32,18 @@ function(tkl_testmodule_init)
   set_property(GLOBAL PROPERTY "tkl::testlib::testmodule::retcode" -1)
   set_property(GLOBAL PROPERTY "tkl::testlib::testcase::retcode" -1)
 
-  if (NOT DEFINED TESTS_ROOT)
-    message(FATAL_ERROR "TESTS_ROOT must be defined")
+  set_property(GLOBAL PROPERTY "tkl::testlib::testcase::func" "")
+
+  if (NOT DEFINED TESTS_ROOT OR NOT IS_DIRECTORY "${TESTS_ROOT}")
+    message(FATAL_ERROR "TESTS_ROOT variable must be defained externally before include this module: TESTS_ROOT=`${TESTS_ROOT}`")
   endif()
 
-  if (NOT DEFINED TACKLELIB_TESTLIB_TESTMODULE_FILE)
-    message(FATAL_ERROR "TACKLELIB_TESTLIB_TESTMODULE_FILE must be defined")
+  if (NOT DEFINED TACKLELIB_TESTLIB_TESTMODULE_FILE OR NOT EXISTS "${TACKLELIB_TESTLIB_TESTMODULE_FILE}" OR IS_DIRECTORY "${TACKLELIB_TESTLIB_TESTMODULE_FILE}")
+    message(FATAL_ERROR "TACKLELIB_TESTLIB_TESTMODULE_FILE file path must exist before include this module: TACKLELIB_TESTLIB_TESTMODULE_FILE=`${TACKLELIB_TESTLIB_TESTMODULE_FILE}`")
   endif()
+
+  file(RELATIVE_PATH TACKLELIB_TESTLIB_TESTMODULE_FILE_REL "${TESTS_ROOT}" "${TACKLELIB_TESTLIB_TESTMODULE_FILE}")
+  set_property(GLOBAL PROPERTY "tkl::testlib::testmodule::file" "${TACKLELIB_TESTLIB_TESTMODULE_FILE_REL}")
 
   # CAUTION:
   #   Must use global property here to avoid accidental misuse, because a variable existence would depend on a function context.
@@ -76,31 +81,21 @@ function(tkl_testmodule_run_test_cases)
     message(FATAL_ERROR "Test module process is not initialized properly, call to `RunTestModule.cmake` to initialize and execute the test module process")
   endif()
 
+  tkl_get_global_prop(TACKLELIB_TESTLIB_TESTMODULE_FILE_REL "tkl::testlib::testmodule::file" 1)
+
   foreach(test_func ${ARGN})
     tkl_eval("\
-if (NOT TESTS_ROOT OR NOT IS_DIRECTORY \"$\\{TESTS_ROOT}\")
-  message(FATAL_ERROR \"TESTS_ROOT variable must be defained externally before include this module: TESTS_ROOT=`$\\{TESTS_ROOT}`\")
-endif()
-
-if (NOT EXISTS \"${CMAKE_CURRENT_LIST_FILE}\" OR IS_DIRECTORY \"${CMAKE_CURRENT_LIST_FILE}\")
-  message(FATAL_ERROR \"CMAKE_CURRENT_LIST_FILE path must exist before include this module: CMAKE_CURRENT_LIST_FILE=`${CMAKE_CURRENT_LIST_FILE}`\")
-endif()
-
-file(RELATIVE_PATH TACKLELIB_TESTLIB_TESTMODULE_FILE \"$\\{TESTS_ROOT}\" \"${CMAKE_CURRENT_LIST_FILE}\")
-set_property(GLOBAL PROPERTY \"tkl::testlib::testmodule::file\" \"\${TACKLELIB_TESTLIB_TESTMODULE_FILE}\") # empty test cases always fail
-
-tkl_set_global_prop_and_var(TACKLELIB_TESTLIB_TESTCASE_FUNC \"tkl::testlib::testcase::func\" \"${test_func}\")
-
 tkl_set_global_prop_and_var(TACKLELIB_TESTLIB_TESTCASE_RETCODE \"tkl::testlib::testcase::retcode\" -1) # empty test cases always fail
+tkl_set_global_prop_and_var(TACKLELIB_TESTLIB_TESTCASE_FUNC \"tkl::testlib::testcase::func\" \"${test_func}\")
 
 ${test_func}()
 
 tkl_get_global_prop(TACKLELIB_TESTLIB_TESTCASE_RETCODE \"tkl::testlib::testcase::retcode\" 1)
 
 if (NOT TACKLELIB_TESTLIB_TESTCASE_RETCODE)
-  tkl_testmodule_print_msg(\"[   OK   ] `\${TACKLELIB_TESTLIB_TESTMODULE_FILE}`: \${TACKLELIB_TESTLIB_TESTCASE_FUNC}\")
+  tkl_testmodule_print_msg(\"[   OK   ] `${TACKLELIB_TESTLIB_TESTMODULE_FILE_REL}`: \${test_func}\")
 else()
-  tkl_testmodule_print_msg(\"[ FAILED ] `\${TACKLELIB_TESTLIB_TESTMODULE_FILE}`: \${TACKLELIB_TESTLIB_TESTCASE_FUNC}\")
+  tkl_testmodule_print_msg(\"[ FAILED ] `${TACKLELIB_TESTLIB_TESTMODULE_FILE_REL}`: \${test_func}\")
 endif()
 
 tkl_testmodule_update_status()
@@ -141,10 +136,14 @@ endif()
       tkl_set_global_prop_and_var(TACKLELIB_TESTLIB_TESTMODULE_RETCODE "tkl::testlib::testmodule::retcode" 0)
     endif()
   else()
-    tkl_get_global_prop(TACKLELIB_TESTLIB_TESTMODULE_FILE "tkl::testlib::testmodule::file" 1)
+    tkl_get_global_prop(TACKLELIB_TESTLIB_TESTMODULE_FILE_REL "tkl::testlib::testmodule::file" 1)
     tkl_get_global_prop(TACKLELIB_TESTLIB_TESTCASE_FUNC "tkl::testlib::testcase::func" 1)
 
-    tkl_testmodule_print_msg("[ ASSERT ] `\${TACKLELIB_TESTLIB_TESTMODULE_FILE}`: ${TACKLELIB_TESTLIB_TESTCASE_FUNC}: if_exp=`${if_exp}` msg=`${msg}`")
+    if (NOT TACKLELIB_TESTLIB_TESTCASE_FUNC STREQUAL "")
+      tkl_testmodule_print_msg("[ ASSERT ] `${TACKLELIB_TESTLIB_TESTMODULE_FILE_REL}`: ${TACKLELIB_TESTLIB_TESTCASE_FUNC}: if_exp=`${if_exp}` msg=`${msg}`")
+    else()
+      tkl_testmodule_print_msg("[ ASSERT ] `${TACKLELIB_TESTLIB_TESTMODULE_FILE_REL}`: if_exp=`${if_exp}` msg=`${msg}`")
+    endif()
     tkl_set_global_prop_and_var(TACKLELIB_TESTLIB_TESTMODULE_RETCODE "tkl::testlib::testmodule::retcode" 1)
     return() # always return from a test case function on first fail
   endif()
--- cmake_tests/_scripts/tests_02_build_mode.bat
+++ cmake_tests/_scripts/tests_02_build_mode.bat
@@ -4,13 +4,11 @@ setlocal
 
 call "%%~dp0__init__.bat" || goto :EOF
 
-set "TACKLELIB_TESTLIB_TESTSCRIPT_FILE=%~n0"
-
 call :CMD cmake ^
   "-DCMAKE_MODULE_PATH=%%TESTS_ROOT%%;%%PROJECT_ROOT%%/cmake" ^
   "-DPROJECT_ROOT=%%PROJECT_ROOT%%" ^
   "-DTESTS_ROOT=%%TESTS_ROOT%%" ^
-  "-DTACKLELIB_TESTLIB_TESTSCRIPT_FILE=%%TACKLELIB_TESTLIB_TESTSCRIPT_FILE%%" ^
+  "-DTACKLELIB_TESTLIB_TESTSCRIPT_FILE=%%TESTS_ROOT%%/%%~n0.cmake" ^
   -P ^
   "%%PROJECT_ROOT%%/cmake/tacklelib/testlib/tools/RunTestLib.cmake" ^
   %%*
