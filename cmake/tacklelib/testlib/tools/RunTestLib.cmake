d15c3e12913ad987388ac50763600384ae516340
andry81 <andry@inbox.ru>
2019-08-04 21:58:51 +0000

    2019.08.04: * fixed: cmake: testlib: `ENABLE_CMAKE_COMMAND_FROM_COMMAND_LIST` variable usage * fixed: cmake: testlib: removed trace messages instead status messages for the cmake 3.15.x and higher binaries targets * fixed: cmake: incomplete comments filter (`SetVarsFromFiles.cmake`) * fixed: cmake: other build errors * fixed: cmake_tests: `ENABLE_CMAKE_COMMAND_FROM_COMMAND_LIST` variable usage * new: cmake_tests: cmake 3.15.1 binaries target * changed: cmake_tests: cmake 3.14.5 binaries is retargeted to cmake 3.14.6 * changed: cmake_tests: cmake 3.15.0-rc1 binaries is retargeted to cmake 3.15.0
    git-svn-id: svn+ssh://svn.code.sf.net/p/tacklelib/tacklelib/trunk@200 ffccfebd-399a-4e7c-9cd2-5fb927d1fecb

--- cmake/tacklelib/testlib/tools/RunTestLib.cmake
+++ cmake/tacklelib/testlib/tools/RunTestLib.cmake
@@ -8,17 +8,36 @@ if (ENABLE_CMAKE_COMMAND_FROM_COMMAND_LIST)
   foreach(CMAKE_COMMAND IN LISTS CMAKE_COMMAND_LIST)
     tkl_testlib_info_msg("CMAKE_COMMAND=`${CMAKE_COMMAND}`")
   endforeach()
+  message("===")
+
+  tkl_testlib_setup_msg("CMAKE_COMMAND=`${CMAKE_COMMAND}`")
+  tkl_testlib_setup_msg(">\${CMAKE_COMMAND} --version")
+  execute_process(
+    COMMAND
+      "${CMAKE_COMMAND}"
+      --version
+  )
+  message("---")
+
+  tkl_make_var_from_CMAKE_ARGV_ARGC(cmake_argv)
+  #message("cmake_argv=${cmake_argv}")
+
+  tkl_list_sublist(cmake_argv_tail 1 -1 cmake_argv)
+  #message("cmake_argv_tail=${cmake_argv_tail}")
+
+  tkl_escape_list_expansion(cmake_args "${cmake_argv_tail}")
+  #message("cmake_args=${cmake_args}")
 
   foreach(CMAKE_COMMAND IN LISTS CMAKE_COMMAND_LIST)
     if (EXISTS "${CMAKE_COMMAND}" AND NOT IS_DIRECTORY "${CMAKE_COMMAND}")
-      tkl_testlib_setup_msg("CMAKE_COMMAND=`${CMAKE_COMMAND}`")
-      tkl_testlib_setup_msg(">\${CMAKE_COMMAND} --version")
+      # We must call the new cmake version from here directly instead of include and pass all respective variables.
       execute_process(
         COMMAND
           "${CMAKE_COMMAND}"
-          --version
+          "-DTACKLELIB_TESTLIB_SKIP_LOAD_VARS=1"        # skip load variables from `tkl_testlib_init` function
+          "-DENABLE_CMAKE_COMMAND_FROM_COMMAND_LIST=0"  # disable recursion
+          ${cmake_args}
       )
-      include("${TACKLELIB_TESTLIB_TESTSCRIPT_FILE}")
     else()
       message(WARNING "CMAKE_COMMAND is not found: CMAKE_COMMAND=`${CMAKE_COMMAND}`")
     endif()
