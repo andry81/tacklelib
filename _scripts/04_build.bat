d3b21e7b6ec5eb8aa18c59c4d56e33ddb1dab908
andry81 <andry@inbox.ru>
2019-08-06 07:59:06 +0000

    2019.08.06: * fixed: scripts: invalid `CMAKE_BUILD_ROOT` variable usage (regression) * fixed: cmake: invalid `CMAKE_BUILD_ROOT` variable usage (regression)
    git-svn-id: svn+ssh://svn.code.sf.net/p/tacklelib/tacklelib/trunk@207 ffccfebd-399a-4e7c-9cd2-5fb927d1fecb

--- _scripts/04_build.bat
+++ _scripts/04_build.bat
@@ -90,7 +90,7 @@ for /F "eol=# tokens=* delims=" %%i in ("!CMAKE_CMD_LINE!") do (
   set "CMAKE_CMD_LINE=%%i"
 )
 
-pushd "%CMAKE_BUILD_ROOT%" && (
+pushd "%CMAKE_BUILD_DIR%" && (
   (
     call :CMD cmake %CMAKE_CMD_LINE%
   ) || ( popd & goto BUILD_END )
