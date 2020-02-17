92f343966524d5127202eaa3e474483b14994864
andry81 <andry@inbox.ru>
2020-02-17 13:32:34 +0000

    2020.02.17: * new: _scripts/tools: `wxFileDialog` utility to select a file/directory in GUI * new: _scripts/tools: `cmd_admin.lnk` shortcut file to promote privileges up to the administrator from a script in the Windows * new: preconfigure scripts
    git-svn-id: svn+ssh://svn.code.sf.net/p/tacklelib/tacklelib/trunk@310 ffccfebd-399a-4e7c-9cd2-5fb927d1fecb

---
+++ 01_preconfigure.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Script ONLY for execution.
+if [[ -n "$BASH" && (-z "$BASH_LINENO" || ${BASH_LINENO[0]} -eq 0) ]]; then 
+
+source "/bin/bash_entry" || exit $?
+tkl_include "__init__.sh" || exit $?
+
+function Call()
+{
+  echo ">$@"
+  echo
+  "$@"
+  LastError=$?
+  return $LastError
+}
+
+function Pause()
+{
+  local key
+  read -n1 -r -p "Press any key to continue..." key
+  echo
+}
+
+case "$OSTYPE" in
+  mingw* | msys* | cygwin*)
+    Call "${COMSPEC//\\//}" /c "$CONFIGURE_ROOT/01_preconfigure.bat" $@
+    exit $?
+    ;;
+  *)
+    echo "1. Download the local third party project: `tacklelib--3dparty`: https://sf.net/p/tacklelib/3dparty"
+    echo "2. Read the instructions in the project readme to checkout and build third party libraries."
+    echo "3. Press any key to continue and select the `_src` subdirectory in the `tacklelib-3dparty` project as a third party catalog."
+
+    Pause
+
+    _3DPARTY_ROOT=$("$UTILITY_ROOT/wxFileDialog" "" "$CONFIGURE_ROOT" "Select the third party catalog to link with..." -de)
+
+    if [[ ! -d "$_3DPARTY_ROOT" ]]; then
+      if [[ -z "$_3DPARTY_ROOT" ]]; then
+        echo "error: $0: third party catalog is not selected." >&2
+      else
+        echo "error: $0: third party catalog does not exist: `$_3DPARTY_ROOT`" >&2
+      fi
+      exit 255
+    fi
+
+    Call ln -s "$CONFIGURE_ROOT/_3dparty" "$_3DPARTY_ROOT"
+
+    #Call ln -s "$CONFIGURE_ROOT/_3dparty/utility/tacklelib/tacklelib/_scripts" "$CONFIGURE_ROOT/_scripts"
+    #Call ln -s "$CONFIGURE_ROOT/_3dparty/utility/tacklelib/tacklelib/cmake" "$CONFIGURE_ROOT/cmake"
+    ;;
+esac
+
+fi
