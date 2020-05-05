c1af6ef02ed93cad51c891c025ff5f3c6ad50ed6
andry81 <andry@inbox.ru>
2020-05-05 22:42:02 +0000

    2020.05.06: * fixed: cpp: linkage error throws `error LNK2019: unresolved external symbol "..."` in the MSVC2017 15.9.19 compiler fix by removing the `operator+` function disable (not required) * changed: cpp: code cleanup
    git-svn-id: svn+ssh://svn.code.sf.net/p/tacklelib/tacklelib/trunk@370 ffccfebd-399a-4e7c-9cd2-5fb927d1fecb

--- src/tackle/path_string.cpp
+++ src/tackle/path_string.cpp
@@ -1,36 +1,22 @@
 #include <tacklelib/tackle/path_string.hpp>
 
 
-#define IMPLEMENT_TEMPLATE_INSTANCE_VARIANT_0(char_) \
-    template class path_basic_string<char_, std::char_traits<char_>, std::allocator<char_>, utility::literal_separators<char_>::forward_slash_char>; \
-    template class path_basic_string<char_, std::char_traits<char_>, std::allocator<char_>, utility::literal_separators<char_>::backward_slash_char>
-
-#if 0
-    template path_basic_string<char_, std::char_traits<char_>, std::allocator<char_>, utility::literal_separators<char_>::forward_slash_char> operator+ ( \
-        path_basic_string<char_, std::char_traits<char_>, std::allocator<char_>, utility::literal_separators<char_>::forward_slash_char> l, \
-        std::basic_string<char_, std::char_traits<char_>, std::allocator<char_> > r); \
-    \
-    template path_basic_string<char_, std::char_traits<char_>, std::allocator<char_>, utility::literal_separators<char_>::forward_slash_char> operator+ ( \
-        std::basic_string<char_, std::char_traits<char_>, std::allocator<char_>> l, \
-        path_basic_string<char_, std::char_traits<char_>, std::allocator<char_>, utility::literal_separators<char_>::forward_slash_char> r); \
-    \
-    template path_basic_string<char_, std::char_traits<char_>, std::allocator<char_>, utility::literal_separators<char_>::forward_slash_char> operator+ ( \
-        path_basic_string<char_, std::char_traits<char_>, std::allocator<char_>, utility::literal_separators<char_>::forward_slash_char> l, \
-        path_basic_string<char_, std::char_traits<char_>, std::allocator<char_>, utility::literal_separators<char_>::forward_slash_char> r); \
-    \
-    template path_basic_string<char_, std::char_traits<char_>, std::allocator<char_>, utility::literal_separators<char_>::forward_slash_char> LIBRARY_API_DECL operator+ ( \
-        path_basic_string<char_, std::char_traits<char_>, std::allocator<char_>, utility::literal_separators<char_>::forward_slash_char> l, \
-        const char_ * p); \
-    \
-    template path_basic_string<char_, std::char_traits<char_>, std::allocator<char_>, utility::literal_separators<char_>::forward_slash_char> LIBRARY_API_DECL operator+ ( \
-        const char_ * p, \
-        path_basic_string<char_, std::char_traits<char_>, std::allocator<char_>, utility::literal_separators<char_>::forward_slash_char> r)
-#endif
+#define TACKLELIB_PATH_STRING_IMPLEMENT_TEMPLATE_INSTANCE_VARIANT_0(char_, literal_separator_type_) \
+    template class path_basic_string<char_, std::char_traits<char_>, std::allocator<char_>, utility::literal_separators<char_>::literal_separator_type_>
 
 namespace tackle
 {
-    IMPLEMENT_TEMPLATE_INSTANCE_VARIANT_0(char);
-    IMPLEMENT_TEMPLATE_INSTANCE_VARIANT_0(wchar_t);
-    IMPLEMENT_TEMPLATE_INSTANCE_VARIANT_0(char16_t);
-    IMPLEMENT_TEMPLATE_INSTANCE_VARIANT_0(char32_t);
+    TACKLELIB_PATH_STRING_IMPLEMENT_TEMPLATE_INSTANCE_VARIANT_0(char, forward_slash_char);
+    TACKLELIB_PATH_STRING_IMPLEMENT_TEMPLATE_INSTANCE_VARIANT_0(char, backward_slash_char);
+
+    TACKLELIB_PATH_STRING_IMPLEMENT_TEMPLATE_INSTANCE_VARIANT_0(wchar_t, forward_slash_char);
+    TACKLELIB_PATH_STRING_IMPLEMENT_TEMPLATE_INSTANCE_VARIANT_0(wchar_t, backward_slash_char);
+
+    TACKLELIB_PATH_STRING_IMPLEMENT_TEMPLATE_INSTANCE_VARIANT_0(char16_t, forward_slash_char);
+    TACKLELIB_PATH_STRING_IMPLEMENT_TEMPLATE_INSTANCE_VARIANT_0(char16_t, backward_slash_char);
+
+    TACKLELIB_PATH_STRING_IMPLEMENT_TEMPLATE_INSTANCE_VARIANT_0(char32_t, forward_slash_char);
+    TACKLELIB_PATH_STRING_IMPLEMENT_TEMPLATE_INSTANCE_VARIANT_0(char32_t, backward_slash_char);
 }
+
+#undef TACKLELIB_PATH_STRING_IMPLEMENT_TEMPLATE_INSTANCE_VARIANT_0 // just in case
