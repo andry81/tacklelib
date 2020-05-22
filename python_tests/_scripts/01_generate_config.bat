e4c69490eb557d6a1bd18a409d64fa3f43c16bf8
andry81 <andry@inbox.ru>
2020-05-22 21:29:52 +0000

    2020.05.23: * new: python_tests: tests auto logging
    git-svn-id: svn+ssh://svn.code.sf.net/p/tacklelib/tacklelib/trunk@390 ffccfebd-399a-4e7c-9cd2-5fb927d1fecb

--- python_tests/_scripts/01_generate_config.bat
+++ python_tests/_scripts/01_generate_config.bat
@@ -4,21 +4,60 @@ rem Configuration variable files generator script.
 
 setlocal
 
+call "%%~dp0__init__/__init0__.bat" || goto INIT_EXIT
+
+if %IMPL_MODE%0 NEQ 0 goto IMPL
+
+rem no local logging if nested call
+set WITH_LOGGING=0
+if %NEST_LVL%0 EQU 0 set WITH_LOGGING=1
+
+if %WITH_LOGGING% EQU 0 goto IMPL
+
+if not exist "%SCRIPTS_LOGS_ROOT%\.log" mkdir "%SCRIPTS_LOGS_ROOT%\.log"
+
+rem use stdout/stderr redirection with logging
+call "%%CONTOOLS_ROOT%%\get_datetime.bat"
+set "LOG_FILE_NAME_SUFFIX=%RETURN_VALUE:~0,4%'%RETURN_VALUE:~4,2%'%RETURN_VALUE:~6,2%_%RETURN_VALUE:~8,2%'%RETURN_VALUE:~10,2%'%RETURN_VALUE:~12,2%''%RETURN_VALUE:~15,3%"
+
+set IMPL_MODE=1
+rem CAUTION:
+rem   We should avoid use handles 3 and 4 while the redirection has take a place because handles does reuse
+rem   internally from left to right when being redirected externally.
+rem   Example: if `1` is redirected, then `3` is internally reused, then if `2` redirected, then `4` is internally reused and so on.
+rem   The discussion of the logic:
+rem   https://stackoverflow.com/questions/9878007/why-doesnt-my-stderr-redirection-end-after-command-finishes-and-how-do-i-fix-i/9880156#9880156
+rem   A partial analisis:
+rem   https://www.dostips.com/forum/viewtopic.php?p=14612#p14612
+rem
+"%COMSPEC%" /C call %0 %* 2>&1 | "%CONTOOLS_ROOT%\wtee.exe" "%SCRIPTS_LOGS_ROOT%\.log\%LOG_FILE_NAME_SUFFIX%.%~n0.log"
+exit /b
+
+:IMPL
 set IN_GENERATOR_SCRIPT=1
 
 call "%%~dp0__init__/__init1__.bat" || goto INIT_EXIT
 
 set /A NEST_LVL+=1
 
+call :MAIN %%*
+set LASTERROR=%ERRORLEVEL%
 
+set /A NEST_LVL-=1
+
+if %NEST_LVL%0 EQU 0 pause
+
+exit /b %LASTERROR%
+
+:MAIN
 set "CMDLINE_SYSTEM_FILE_IN=%PROJECT_ROOT%\python_tests\_config\_scripts\01\%~n0.system%~x0.in"
 
 for %%i in ("%CMDLINE_SYSTEM_FILE_IN%") do (
   set "CMDLINE_FILE_IN=%%i"
-  call :GENERATE || goto EXIT
+  call :GENERATE || exit /b
 )
 
-goto EXIT
+exit /b
 
 :GENERATE
 rem for safe parse
@@ -40,14 +79,7 @@ for /F "eol=# tokens=* delims=" %%i in ("!CMAKE_CMD_LINE!") do (
   set "CMAKE_CMD_LINE=%%i"
 )
 
-pushd "%CMAKE_BUILD_DIR%" && (
-  (
-    call :CMD cmake %CMAKE_CMD_LINE%
-  ) || ( popd & goto GENERATE_END )
-  popd
-)
-
-:GENERATE_END
+call :CMD cmake %CMAKE_CMD_LINE%
 exit /b
 
 :CMD
@@ -58,12 +90,9 @@ echo.
 )
 exit /b
 
-:EXIT
+:INIT_EXIT
 set LASTERROR=%ERRORLEVEL%
 
-set /A NEST_LVL-=1
-
-:INIT_EXIT
 if %NEST_LVL%0 EQU 0 pause
 
 exit /b %LASTERROR%
--- python_tests/_scripts/__init__/__init1__.bat
+++ python_tests/_scripts/__init__/__init1__.bat
@@ -1,5 +1,14 @@
 @echo off
 
+rem CAUTION:
+rem  In case of usage the QtCreator there is set of special broken cases you have to avoid:
+rem  1. Invalid characters in paths: `(`, `)` and `.`.
+rem  2. Non english locale in paths.
+
+if /i "%TACKLELIB_SCRIPTS_INIT1_DIR%" == "%~dp0" exit /b
+
+set "TACKLELIB_SCRIPTS_INIT1_DIR=%~dp0"
+
 if not defined NEST_LVL set NEST_LVL=0
 
 call "%%~dp0__init0__.bat" || exit /b
