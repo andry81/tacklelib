8b3a3035d5344f90c21d0c6dbc46a939b605a126
andry81 <andry@inbox.ru>
2020-05-22 21:29:52 +0000

    2020.05.23: * new: python_tests: tests auto logging
    git-svn-id: svn+ssh://svn.code.sf.net/p/tacklelib/tacklelib/trunk@390 ffccfebd-399a-4e7c-9cd2-5fb927d1fecb

--- python_tests/_scripts/__init__/__init0__.bat
+++ python_tests/_scripts/__init__/__init0__.bat
@@ -1,9 +1,22 @@
 @echo off
 
+rem CAUTION:
+rem  In case of usage the QtCreator there is set of special broken cases you have to avoid:
+rem  1. Invalid characters in paths: `(`, `)` and `.`.
+rem  2. Non english locale in paths.
+
+if /i "%TACKLELIB_SCRIPTS_INIT0_DIR%" == "%~dp0" exit /b
+
+set "TACKLELIB_SCRIPTS_INIT0_DIR=%~dp0"
+
+if not defined NEST_LVL set NEST_LVL=0
+
 call :CANONICAL_PATH PROJECT_ROOT "%%~dp0..\..\.."
+call :CANONICAL_PATH CONTOOLS_ROOT "%%~dp0..\tools"
 call :CANONICAL_PATH TESTS_ROOT "%%PROJECT_ROOT%%/python_tests"
 call :CANONICAL_PATH TACKLELIB_ROOT "%%PROJECT_ROOT%%/python/tacklelib"
 call :CANONICAL_PATH CMDOPLIB_ROOT "%%PROJECT_ROOT%%/python/cmdoplib"
+call :CANONICAL_PATH SCRIPTS_LOGS_ROOT "%%TESTS_ROOT%%"
 
 set "CONFIG_VARS_SYSTEM_FILE_IN=%PROJECT_ROOT%/python_tests/_config/environment_system.vars.in"
 set "CONFIG_VARS_SYSTEM_FILE=%PROJECT_ROOT%/python_tests/_config/environment_system.vars"
--- python_tests/_scripts/__init__/__init1__.sh
+++ python_tests/_scripts/__init__/__init1__.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-# Script can be ONLY included by "source" command.
-if [[ -n "$BASH" && (-z "$BASH_LINENO" || ${BASH_LINENO[0]} -gt 0) ]] && (( ! ${#SOURCE_ROOT_INIT1_SH} )); then 
+# CAUTION:
+#  In case of usage the QtCreator there is set of special broken cases you have to avoid:
+#  1. Invalid characters in paths: `(`, `)` and `.`.
+#  2. Non english locale in paths.
 
-tkl_include "__init0__.sh" || exit $?
+# Script can be ONLY included by "source" command.
+if [[ -n "$BASH" && (-z "$BASH_LINENO" || BASH_LINENO[0] -gt 0) && "$TACKLELIB_SCRIPTS_INIT1_DIR" != "$BASH_SOURCE_DIR" ]]; then 
+
+TACKLELIB_SCRIPTS_INIT1_DIR="$BASH_SOURCE_DIR" # including guard
+
+tkl_include "__init0__.sh" || return $?
 
 # optional compare in case of generator script
-CallAndPrintIf "(( INIT_VERBOSE ))" CheckConfigVersion "$IN_GENERATOR_SCRIPT" \
-  "$CONFIG_VARS_SYSTEM_FILE_IN" "$CONFIG_VARS_SYSTEM_FILE" || Exit
+tkl_call_and_print_if "(( INIT_VERBOSE ))" CheckConfigVersion "$IN_GENERATOR_SCRIPT" \
+  "$CONFIG_VARS_SYSTEM_FILE_IN" "$CONFIG_VARS_SYSTEM_FILE" || return $?
 
 fi
