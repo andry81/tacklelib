df5fec7034d41519147c28421b6b890dc628b2db
andry81 <andry@inbox.ru>
2021-06-15 16:12:21 +0000

    2021.06.15: * new: include/tacklelib/utility/type_traits: `for_each_unroll` for multidimensional arrays
    git-svn-id: svn+ssh://svn.code.sf.net/p/tacklelib/tacklelib/trunk@456 ffccfebd-399a-4e7c-9cd2-5fb927d1fecb

--- include/tacklelib/utility/type_traits.hpp
+++ include/tacklelib/utility/type_traits.hpp
@@ -461,7 +461,7 @@ namespace utility
     struct call_traits<T[N]>
     {
     private:
-       using array_type = T[N];
+       using array_type       = T[N];
     public:
        using value_type       = const T *;
        using reference        = array_type &;
@@ -540,6 +540,99 @@ namespace utility
     {
         f(std::get<I>(t));
         for_each<I + 1, Functor, Args...>(t, std::forward<Functor>(f));
+    }
+
+    // Unrolled `for_each` for multidimensional arrays
+
+    namespace detail
+    {
+        template<bool is_array>
+        struct _for_each_unroll
+        {
+            template <typename Functor, typename T, std::size_t N>
+            _for_each_unroll(T (& arr)[N], Functor && f)
+            {
+                invoke(arr, std::forward<Functor>(f));
+            }
+
+            template <typename Functor, typename T, std::size_t N>
+            _for_each_unroll(T (&& arr)[N], Functor && f)
+            {
+                invoke(std::forward<T[N]>(arr), std::forward<Functor>(f));
+            }
+
+            template <std::size_t I = 0, typename Functor, typename T, std::size_t N>
+            FORCE_INLINE typename std::enable_if<I == N, void>::type
+                invoke(T (& arr)[N], Functor && f)
+            {
+            }
+
+            template <std::size_t I = 0, typename Functor, typename T, std::size_t N>
+            FORCE_INLINE typename std::enable_if<I == N, void>::type
+                invoke(T (&& arr)[N], Functor && f)
+            {
+            }
+
+            template <std::size_t I = 0, typename Functor, typename T, std::size_t N>
+            FORCE_INLINE typename std::enable_if<I < N, void>::type
+                invoke(T (& arr)[N], Functor && f)
+            {
+                _for_each_unroll<std::is_array<T>::value>{ arr[I], std::forward<Functor>(f) };
+                invoke<I + 1, Functor, T, N>(arr, std::forward<Functor>(f));
+            }
+
+            template <std::size_t I = 0, typename Functor, typename T, std::size_t N>
+            FORCE_INLINE typename std::enable_if<I < N, void>::type
+                invoke(T (&& arr)[N], Functor && f)
+            {
+                _for_each_unroll<std::is_array<T>::value>{ std::forward<T>(arr[I]), std::forward<Functor>(f) };
+                invoke<I + 1, Functor, T, N>(arr, std::forward<Functor>(f));
+            }
+        };
+
+        template<>
+        struct _for_each_unroll<false>
+        {
+            template <typename Functor, typename T>
+            _for_each_unroll(const T & value, Functor && f)
+            {
+                f(value);
+            }
+
+            template <typename Functor, typename T>
+            _for_each_unroll(T && value, Functor && f)
+            {
+                f(std::forward<T>(value));
+            }
+        };
+    }
+
+    template <std::size_t I = 0, typename Functor, typename T, std::size_t N>
+    FORCE_INLINE typename std::enable_if<I == N, void>::type
+        for_each_unroll(T (& arr)[N], Functor && f)
+    {
+    }
+
+    template <std::size_t I = 0, typename Functor, typename T, std::size_t N>
+    FORCE_INLINE typename std::enable_if<I == N, void>::type
+        for_each_unroll(T (&& arr)[N], Functor && f)
+    {
+    }
+
+    template <std::size_t I = 0, typename Functor, typename T, std::size_t N>
+    FORCE_INLINE typename std::enable_if<I < N, void>::type
+        for_each_unroll(T (& arr)[N], Functor && f)
+    {
+        detail::_for_each_unroll<std::is_array<T>::value>{ arr[I], std::forward<Functor>(f) };
+        for_each_unroll<I + 1, Functor, T, N>(arr, std::forward<Functor>(f));
+    }
+
+    template <std::size_t I = 0, typename Functor, typename T, std::size_t N>
+    FORCE_INLINE typename std::enable_if<I < N, void>::type
+        for_each_unroll(T (&& arr)[N], Functor && f)
+    {
+        detail::_for_each_unroll<std::is_array<T>::value>{ std::forward<T>(arr[I]), std::forward<Functor>(f) };
+        for_each_unroll<I + 1, Functor, T, N>(std::forward<T[N]>(arr), std::forward<Functor>(f));
     }
 
     // `is_callable` implementation.
