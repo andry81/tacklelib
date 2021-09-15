8901c6c3ac6342d1087368ef620a9d00b434d5f8
andry81 <andry@inbox.ru>
2021-09-15 01:15:48 +0000

    2021.09.15: * changed: _build: admin, deploy: switched to `bash_tacklelib` library usage
    git-svn-id: svn+ssh://svn.code.sf.net/p/tacklelib/tacklelib/trunk@468 ffccfebd-399a-4e7c-9cd2-5fb927d1fecb

--- _build/deploy/collect_ldd_deps.sh
+++ _build/deploy/collect_ldd_deps.sh
@@ -1,123 +1,19 @@
 #!/bin/bash
 
 # Script ONLY for execution.
-if [[ -n "$BASH" && (-z "$BASH_LINENO" || ${BASH_LINENO[0]} -eq 0) ]]; then 
+if [[ -n "$BASH" && (-z "$BASH_LINENO" || ${BASH_LINENO[0]} -eq 0) ]]; then
 
-# WORKAROUND:
-#   The `declare -g` has been introduced in the `bash-4.2-alpha`, so to make
-#   a global variable in an older version we have to replace the
-#   `declare -g` by a sequence of calls to `unset` and `eval`.
-#
-#   The `tkl_return_local` has used for both issues:
-#   1. To return a local variable.
-#   2. To replace the `declare -g`.
-#
-function tkl_return_local()
-{
-  unset $1 # must be local
-  eval "$1=\"\$2\""
-}
-
-function ScriptBaseInit()
-{
-  if [[ -n "$BASH_LINENO" ]] && (( ${BASH_LINENO[${#BASH_LINENO[@]}-1]} > 0 )); then
-    local ScriptFilePath="${BASH_SOURCE[${#BASH_LINENO[@]}-1]//\\//}"
-  else
-    local ScriptFilePath="${0//\\//}"
-  fi
-
-  tkl_get_abs_path_from_dir "$ScriptFilePath"
-  ScriptFilePath="${RETURN_VALUE}"
-
-  local ScriptDirPath="${ScriptFilePath%[/]*}"
-  local ScriptFileName="${ScriptFilePath##*[/]}"
-
-  tkl_return_local ScriptFilePath "${ScriptFilePath}"
-  tkl_return_local ScriptDirPath "${ScriptDirPath}"
-  tkl_return_local ScriptFileName "${ScriptFileName}"
-}
-
-function tkl_get_abs_path_from_dir()
-{
-  # drop return value
-  RETURN_VALUE="$1"
-
-  local DirPath="$1"
-  local RelativePath="$2"
-
-  # WORKAROUND:
-  #   Because some versions of readlink can not handle windows native absolute
-  #   paths correctly, then always try to convert directory path to a backend
-  #   path before the readlink in case if the path has specific native path
-  #   characters.
-  if [[ "${DirPath:1:1}" == ":" ]]; then
-    tkl_convert_native_path_to_backend "$DirPath"
-    DirPath="$RETURN_VALUE"
-  fi
-
-  if [[ -n "$DirPath" && -x "/bin/readlink" ]]; then
-    if [[ "${RelativePath:0:1}" != '/' ]]; then
-      RETURN_VALUE="`/bin/readlink -m "$DirPath${RelativePath:+/}$RelativePath"`"
-    else
-      RETURN_VALUE="`/bin/readlink -m "$RelativePath"`"
-    fi
-  fi
-
-  return 0
-}
-
-function tkl_convert_native_path_to_backend()
-{
-  # drop return value
-  RETURN_VALUE="$1"
-
-  # Convert all back slashes to slashes.
-  local PathToConvert="${1//\\//}"
-
-  [[ -n "$PathToConvert" ]] || return 1
-
-  # workaround for the bash 3.1.0 bug for the expression "${arg:X:Y}",
-  # where "Y == 0" or "Y + X >= ${#arg}"
-  local PathToConvertLen=${#PathToConvert}
-  local PathPrefixes=('' '')
-  local PathSuffix=""
-  if (( PathToConvertLen > 0 )); then
-    PathPrefixes[0]="${PathToConvert:0:1}"
-  fi
-  if (( PathToConvertLen > 1 )); then
-    PathPrefixes[1]="${PathToConvert:1:1}"
-  fi
-  if (( PathToConvertLen >= 3 )); then
-    PathSuffix="${PathToConvert:2}"
-  fi
-  PathSuffix="${PathSuffix%/}"
-
-  # Convert path drive prefix too.
-  if [[ "${PathPrefixes[0]}" != '/' && "${PathPrefixes[0]}" != '.' && "${PathPrefixes[1]}" == ':' ]]; then
-    case "$OSTYPE" in
-      cygwin*) PathToConvert="/cygdrive/${PathPrefixes[0]}$PathSuffix" ;;
-      *)
-        PathToConvert="/${PathPrefixes[0]}$PathSuffix"
-        # add slash to the end of path in case of drive only path
-        (( ! ${#PathSuffix} )) && PathToConvert="$PathToConvert/"
-      ;;
-    esac
-  fi
-
-  RETURN_VALUE="$PathToConvert"
-
-  return 0
-}
-
-ScriptBaseInit "$@"
+source '/bin/bash_tacklelib' || exit $?
+tkl_include '../__init__/__init__.sh' || tkl_abort_include
 
 if [[ "$1" == "--help" || "$1" == "-h" ]]; then
   echo "Usage: <SearchRoot> <File1>[:<File2>[:...[:<FileN>]]] <OutDepsFile> <OutDepsDir>"
-  echo "Example: $ScriptFileName .. \"*.so:*.so.*\" deps.lst ."
+  echo "Example: $BASH_SOURCE_FILE_NAME .. \"*.so:*.so.*\" deps.lst ."
   exit 1
 fi
 
-APP_ROOT="`readlink -f "$ScriptDirPath/.."`"
+tkl_normalize_path -a -s "$BASH_SOURCE_DIR/../.."
+APP_ROOT="$RETURN_VALUE"
 
 SEARCH_ROOT_LIST="$1"     # directory path list where to start search files dependencies not recursively
 FILE_LIST_TO_FIND="$2"    # `:`-separated list of wildcard case insensitive file names or file paths to find
@@ -129,13 +25,13 @@ OUT_DEPS_DIR="$6"         # directory there to copy found dependencies
 
 if [[ -n "$OUT_DEPS_FILE" ]]; then
   touch "$OUT_DEPS_FILE" 2> /dev/null || {
-    echo "$ScriptFileName: error: cannot create OUT_DEPS_FILE file: \"$OUT_DEPS_FILE\"."
+    echo "$BASH_SOURCE_FILE_NAME: error: cannot create OUT_DEPS_FILE file: \"$OUT_DEPS_FILE\"."
     exit 2
   } 1>&2
 fi
 
 if [[ ! -d "$OUT_DEPS_DIR" ]]; then
-  echo "$ScriptFileName: error: directory OUT_DEPS_DIR is not found: \"$OUT_DEPS_DIR\"." >&2
+  echo "$BASH_SOURCE_FILE_NAME: error: directory OUT_DEPS_DIR is not found: \"$OUT_DEPS_DIR\"." >&2
   exit 3
 fi
 
@@ -144,24 +40,6 @@ if [[ ! -d "$CWD" ]]; then
 else
   CWD="`readlink -f "$CWD"`"
 fi
-
-function Call()
-{
-  echo ">$@"
-  "$@"
-  LastError=$?
-  return $LastError
-}
-
-function Pushd()
-{
-  pushd "$@" > /dev/null
-}
-
-function Popd()
-{
-  popd "$@" > /dev/null
-}
 
 function FindFiles()
 {
@@ -177,7 +55,7 @@ function FindFiles()
 
   i=0
   local IFS=$':\t\r\n'; for search_root in $SEARCH_ROOT_LIST; do
-    if Pushd "$search_root"; then
+    if tkl_pushd "$search_root"; then
       local IFS=$':\t\r\n'; for file in $FILE_LIST_TO_FIND; do
         if [[ -f "$file" ]]; then
           file_list_to_find[i++]="$file"
@@ -185,15 +63,15 @@ function FindFiles()
           (( i++ ))
         fi
       done
-      Popd
+      tkl_popd
     else
-      echo "$ScriptFileName: error: search root is not found: \"$search_root\"." >&2
+      echo "$BASH_SOURCE_FILE_NAME: error: search root is not found: \"$search_root\"." >&2
       return 1
     fi
   done
 
   (( ! ${#file_list_to_find[@]} )) && {
-    echo "$ScriptFileName: warning: file search list is empty." >&2
+    echo "$BASH_SOURCE_FILE_NAME: warning: file search list is empty." >&2
     return 2
   }
 
@@ -466,7 +344,7 @@ function CopyFile()
       done
 
       if (( ${#copy_to_list[@]} )); then
-        Call cp "${flag_args[@]}" "$file" "${copy_to_list[@]}" || return $?
+        tkl_call cp "${flag_args[@]}" "$file" "${copy_to_list[@]}" || return $?
       fi
     elif [[ -L "$file" ]]; then
       if (( create_symlinks )); then
@@ -494,9 +372,9 @@ function CopyFile()
               fi
               copy_to_file_dir="$RETURN_VALUE"
 
-              Pushd "$copy_to_file_dir" && {
+              tkl_pushd "$copy_to_file_dir" && {
                 MakeSymlink -I "$file_name" "$link_file_name"
-                Popd
+                tkl_popd
               }
             done
           fi
@@ -549,7 +427,8 @@ function RemoveItemFromUArray()
   done
 
   return 1
-} 
+}
+
 function CollectLddDeps()
 {
   local LDD_TOOL=ldd #alternative: `lddtee`
@@ -567,7 +446,7 @@ function CollectLddDeps()
   (( LastError == 2 )) && return 0
 
   if (( ! ${#file_list_found[@]} )); then
-    echo "$ScriptFileName: info: nothing to search." >&2
+    echo "$BASH_SOURCE_FILE_NAME: info: nothing to search." >&2
     return 10
   fi
 
@@ -628,7 +507,7 @@ function CollectLddDeps()
       if [[ "$link_file_dir" != "$ToDir" ]]; then
         if [[ -f "$ToDir/$link_file_name" ]]; then
           if ! cmp "$link_file" "$ToDir/$link_file_name" > /dev/null; then
-            echo "$ScriptFileName: error: being copied dependency file is already exist with different content: \"$LinkName\" -> \"$link_file\" copy to \"$ToDir/\"" >&2
+            echo "$BASH_SOURCE_FILE_NAME: error: being copied dependency file is already exist with different content: \"$LinkName\" -> \"$link_file\" copy to \"$ToDir/\"" >&2
             return 1
           fi
           return 255
@@ -731,8 +610,8 @@ function CollectLddDeps()
     eval $SHELLNOCASEMATCH
 
     if (( ${#not_found_lib_list[@]} )); then
-      echo "$ScriptFileName: error: having not found dependencies." >&2
-      echo "$ScriptFileName: info: not found dependencies list:"
+      echo "$BASH_SOURCE_FILE_NAME: error: having not found dependencies." >&2
+      echo "$BASH_SOURCE_FILE_NAME: info: not found dependencies list:"
 
       for link_name in "${not_found_lib_list[@]}"; do
         echo "  ${link_name}"
--- _build/deploy/create_links.sh
+++ _build/deploy/create_links.sh
@@ -1,156 +1,22 @@
 #!/bin/bash
 
 # Script ONLY for execution.
-if [[ -n "$BASH" && (-z "$BASH_LINENO" || ${BASH_LINENO[0]} -eq 0) ]]; then 
+if [[ -n "$BASH" && (-z "$BASH_LINENO" || ${BASH_LINENO[0]} -eq 0) ]]; then
 
-# WORKAROUND:
-#   The `declare -g` has been introduced in the `bash-4.2-alpha`, so to make
-#   a global variable in an older version we have to replace the
-#   `declare -g` by a sequence of calls to `unset` and `eval`.
-#
-#   The `tkl_return_local` has used for both issues:
-#   1. To return a local variable.
-#   2. To replace the `declare -g`.
-#
-function tkl_return_local()
-{
-  unset $1 # must be local
-  eval "$1=\"\$2\""
-}
+source '/bin/bash_tacklelib' || exit $?
+tkl_include '../__init__/__init__.sh' || tkl_abort_include
 
-function ScriptBaseInit()
-{
-  if [[ -n "$BASH_LINENO" ]] && (( ${BASH_LINENO[${#BASH_LINENO[@]}-1]} > 0 )); then
-    local ScriptFilePath="${BASH_SOURCE[${#BASH_LINENO[@]}-1]//\\//}"
-  else
-    local ScriptFilePath="${0//\\//}"
-  fi
-
-  tkl_get_abs_path_from_dir "$ScriptFilePath"
-  ScriptFilePath="${RETURN_VALUE}"
-
-  local ScriptDirPath="${ScriptFilePath%[/]*}"
-  local ScriptFileName="${ScriptFilePath##*[/]}"
-
-  tkl_return_local ScriptFilePath "${ScriptFilePath}"
-  tkl_return_local ScriptDirPath "${ScriptDirPath}"
-  tkl_return_local ScriptFileName "${ScriptFileName}"
-}
-
-function tkl_get_abs_path_from_dir()
-{
-  # drop return value
-  RETURN_VALUE="$1"
-
-  local DirPath="$1"
-  local RelativePath="$2"
-
-  # WORKAROUND:
-  #   Because some versions of readlink can not handle windows native absolute
-  #   paths correctly, then always try to convert directory path to a backend
-  #   path before the readlink in case if the path has specific native path
-  #   characters.
-  if [[ "${DirPath:1:1}" == ":" ]]; then
-    tkl_convert_native_path_to_backend "$DirPath"
-    DirPath="$RETURN_VALUE"
-  fi
-
-  if [[ -n "$DirPath" && -x "/bin/readlink" ]]; then
-    if [[ "${RelativePath:0:1}" != '/' ]]; then
-      RETURN_VALUE="`/bin/readlink -m "$DirPath${RelativePath:+/}$RelativePath"`"
-    else
-      RETURN_VALUE="`/bin/readlink -m "$RelativePath"`"
-    fi
-  fi
-
-  return 0
-}
-
-function tkl_convert_native_path_to_backend()
-{
-  # drop return value
-  RETURN_VALUE="$1"
-
-  # Convert all back slashes to slashes.
-  local PathToConvert="${1//\\//}"
-
-  [[ -n "$PathToConvert" ]] || return 1
-
-  # workaround for the bash 3.1.0 bug for the expression "${arg:X:Y}",
-  # where "Y == 0" or "Y + X >= ${#arg}"
-  local PathToConvertLen=${#PathToConvert}
-  local PathPrefixes=('' '')
-  local PathSuffix=""
-  if (( PathToConvertLen > 0 )); then
-    PathPrefixes[0]="${PathToConvert:0:1}"
-  fi
-  if (( PathToConvertLen > 1 )); then
-    PathPrefixes[1]="${PathToConvert:1:1}"
-  fi
-  if (( PathToConvertLen >= 3 )); then
-    PathSuffix="${PathToConvert:2}"
-  fi
-  PathSuffix="${PathSuffix%/}"
-
-  # Convert path drive prefix too.
-  if [[ "${PathPrefixes[0]}" != '/' && "${PathPrefixes[0]}" != '.' && "${PathPrefixes[1]}" == ':' ]]; then
-    case "$OSTYPE" in
-      cygwin*) PathToConvert="/cygdrive/${PathPrefixes[0]}$PathSuffix" ;;
-      *)
-        PathToConvert="/${PathPrefixes[0]}$PathSuffix"
-        # add slash to the end of path in case of drive only path
-        (( ! ${#PathSuffix} )) && PathToConvert="$PathToConvert/"
-      ;;
-    esac
-  fi
-
-  RETURN_VALUE="$PathToConvert"
-
-  return 0
-}
-
-ScriptBaseInit "$@"
-
-if [[ ! -f "$ScriptDirPath/user_links.lst" ]]; then
-  echo "$ScriptFileName: error: \"user_links.lst\" must exist in the script directory: \"$ScriptDirPath/\"" >&2
+if [[ ! -f "$BASH_SOURCE_DIR/user_links.lst" ]]; then
+  echo "$BASH_SOURCE_FILE_NAME: error: \"user_links.lst\" must exist in the script directory: \"$BASH_SOURCE_DIR/\"" >&2
   exit 1
 fi
 
-function ReadCommandLineFlags()
-{
-  local out_args_list_name_var="$1"
-  shift
-
-  local args
-  args=("$@")
-  local args_len=${#@}
-
-  local i
-  local j
-
-  j=0
-  for (( i=0; i < $args_len; i++ )); do
-    # collect all flag arguments until first not flag
-    if (( ${#args[i]} )); then
-      if [[ "${args[i]#-}" != "${args[i]}" ]]; then
-        eval "$out_args_list_name_var[j++]=\"\${args[i]}\""
-        shift
-      else
-        break
-      fi
-    else
-      # stop on empty string too
-      break
-    fi
-  done
-}
-
 flag_args=()
 
-ReadCommandLineFlags flag_args "$@"
+tkl_read_command_line_flags flag_args "$@"
 (( ${#flag_args[@]} )) && shift ${#flag_args[@]}
 
-APP_ROOT="`readlink -f "$ScriptDirPath/../.."`"
+APP_ROOT="`readlink -f "$BASH_SOURCE_DIR/../.."`"
 APP_DIR_LIST=("$APP_ROOT" "$APP_ROOT/lib")
 
 CONFIGURE_ROOT="$1"
@@ -160,7 +26,7 @@ if [[ -n "$CONFIGURE_ROOT" ]]; then
     CONFIGURE_ROOT="`readlink -f "$CONFIGURE_ROOT"`"
     APP_DIR_LIST=("$CONFIGURE_ROOT" "$CONFIGURE_ROOT/lib")
   else
-    echo "$ScriptFileName: error: input directory is not found: \"$CONFIGURE_ROOT\"."
+    echo "$BASH_SOURCE_FILE_NAME: error: input directory is not found: \"$CONFIGURE_ROOT\"."
     exit 2
   fi
 fi
@@ -174,13 +40,13 @@ for flag in "${flag_args[@]}"; do
   fi
 done
 
-if (( ! create_user_symlinks_only )) && [[ ! -f "$ScriptDirPath/gen_links.lst" ]]; then
-  echo "$ScriptFileName: error: \"gen_links.lst\" must exist in the script directory." >&2
+if (( ! create_user_symlinks_only )) && [[ ! -f "$BASH_SOURCE_DIR/gen_links.lst" ]]; then
+  echo "$BASH_SOURCE_FILE_NAME: error: \"gen_links.lst\" must exist in the script directory." >&2
   exit 3
 fi
 
 # create user links at first
-echo "Creating user links from \"$ScriptDirPath/user_links.lst\"..."
+echo "Creating user links from \"$BASH_SOURCE_DIR/user_links.lst\"..."
 num_links=0
 for app_dir in "${APP_DIR_LIST[@]}"; do
   [[ ! -d "$app_dir" ]] && continue
@@ -192,7 +58,7 @@ for app_dir in "${APP_DIR_LIST[@]}"; do
         ln -s "$RefPath" "$LinkPath"
         (( num_links++ ))
       fi
-    done < "$ScriptDirPath/user_links.lst"
+    done < "$BASH_SOURCE_DIR/user_links.lst"
     popd > /dev/null
   }
 done
@@ -201,7 +67,7 @@ done
 
 if (( ! create_user_symlinks_only )); then
   # create generated links
-  echo "Creating generated links from \"$ScriptDirPath/gen_links.lst\"..."
+  echo "Creating generated links from \"$BASH_SOURCE_DIR/gen_links.lst\"..."
   for app_dir in "${APP_DIR_LIST[@]}"; do
     [[ ! -d "$app_dir" ]] && continue
     pushd "$app_dir" > /dev/null && {
@@ -210,7 +76,7 @@ if (( ! create_user_symlinks_only )); then
           echo "  '$LinkPath' -> '$RefPath'"
           ln -s "$RefPath" "$LinkPath"
         fi
-      done < "$ScriptDirPath/gen_links.lst"
+      done < "$BASH_SOURCE_DIR/gen_links.lst"
       popd > /dev/null
     }
   done
