a9b2eb6d0985414c2e7dedcce013ed5788c581be
andry81 <andry@inbox.ru>
2021-09-16 22:30:56 +0000

    2021.09.17: * changed: deploy/approot: switched to `bash_tacklelib` library usage
    git-svn-id: svn+ssh://svn.code.sf.net/p/tacklelib/tacklelib/trunk@469 ffccfebd-399a-4e7c-9cd2-5fb927d1fecb

--- deploy/approot/${PROJECT_NAME}.sh
+++ deploy/approot/${PROJECT_NAME}.sh
@@ -1,117 +1,56 @@
 #!/bin/bash
 
+# Description:
+#   An application runner to fix application library dependecies and log the output.
+#
+
 # Script ONLY for execution.
 if [[ -n "$BASH" && (-z "$BASH_LINENO" || ${BASH_LINENO[0]} -eq 0) ]]; then 
 
-# WORKAROUND:
-#   The `declare -g` has been introduced in the `bash-4.2-alpha`, so to make
-#   a global variable in an older version we have to replace the
-#   `declare -g` by a sequence of calls to `unset` and `eval`.
-#
-#   The `tkl_return_local` has used for both issues:
-#   1. To return a local variable.
-#   2. To replace the `declare -g`.
-#
-function tkl_return_local()
-{
-  unset $1 # must be local
-  eval "$1=\"\$2\""
-}
+source '/bin/bash_tacklelib' || exit $?
 
-function ScriptBaseInit()
-{
-  if [[ -n "$BASH_LINENO" ]] && (( ${BASH_LINENO[${#BASH_LINENO[@]}-1]} > 0 )); then
-    local ScriptFilePath="${BASH_SOURCE[${#BASH_LINENO[@]}-1]//\\//}"
-  else
-    local ScriptFilePath="${0//\\//}"
-  fi
+# no local logging if nested call
+(( ! IMPL_MODE && ! NEST_LVL )) && {
+  export IMPL_MODE=1
+  exec 3>&1 4>&2
+  trap 'exec 2>&4 1>&3' EXIT
 
-  tkl_get_abs_path_from_dir "$ScriptFilePath"
-  ScriptFilePath="${RETURN_VALUE}"
+  # date time request base on: https://stackoverflow.com/questions/1401482/yyyy-mm-dd-format-date-in-shell-script/1401495#1401495
+  #
 
-  local ScriptDirPath="${ScriptFilePath%[/]*}"
-  local ScriptFileName="${ScriptFilePath##*[/]}"
-
-  tkl_return_local ScriptFilePath "${ScriptFilePath}"
-  tkl_return_local ScriptDirPath "${ScriptDirPath}"
-  tkl_return_local ScriptFileName "${ScriptFileName}"
-}
-
-function tkl_get_abs_path_from_dir()
-{
-  # drop return value
-  RETURN_VALUE="$1"
-
-  local DirPath="$1"
-  local RelativePath="$2"
-
-  # WORKAROUND:
-  #   Because some versions of readlink can not handle windows native absolute
-  #   paths correctly, then always try to convert directory path to a backend
-  #   path before the readlink in case if the path has specific native path
-  #   characters.
-  if [[ "${DirPath:1:1}" == ":" ]]; then
-    tkl_convert_native_path_to_backend "$DirPath"
-    DirPath="$RETURN_VALUE"
-  fi
-
-  if [[ -n "$DirPath" && -x "/bin/readlink" ]]; then
-    if [[ "${RelativePath:0:1}" != '/' ]]; then
-      RETURN_VALUE="`/bin/readlink -m "$DirPath${RelativePath:+/}$RelativePath"`"
-    else
-      RETURN_VALUE="`/bin/readlink -m "$RelativePath"`"
-    fi
-  fi
-
-  return 0
-}
-
-function tkl_convert_native_path_to_backend()
-{
-  # drop return value
-  RETURN_VALUE="$1"
-
-  # Convert all back slashes to slashes.
-  local PathToConvert="${1//\\//}"
-
-  [[ -n "$PathToConvert" ]] || return 1
-
-  # workaround for the bash 3.1.0 bug for the expression "${arg:X:Y}",
-  # where "Y == 0" or "Y + X >= ${#arg}"
-  local PathToConvertLen=${#PathToConvert}
-  local PathPrefixes=('' '')
-  local PathSuffix=""
-  if (( PathToConvertLen > 0 )); then
-    PathPrefixes[0]="${PathToConvert:0:1}"
-  fi
-  if (( PathToConvertLen > 1 )); then
-    PathPrefixes[1]="${PathToConvert:1:1}"
-  fi
-  if (( PathToConvertLen >= 3 )); then
-    PathSuffix="${PathToConvert:2}"
-  fi
-  PathSuffix="${PathSuffix%/}"
-
-  # Convert path drive prefix too.
-  if [[ "${PathPrefixes[0]}" != '/' && "${PathPrefixes[0]}" != '.' && "${PathPrefixes[1]}" == ':' ]]; then
-    case "$OSTYPE" in
-      "cygwin") PathToConvert="/cygdrive/${PathPrefixes[0]}$PathSuffix" ;;
+  function GetTime()
+  {
+    # RANDOM instead of milliseconds
+    case $BASH_VERSION in
+      # < 4.2
+      [123].* | 4.[01] | 4.0* | 4.1[^0-9]*)
+        PROJECT_LOG_FILE_NAME_SUFFIX=$(date "+%Y'%m'%d_%H'%M'%S''")$(( RANDOM % 1000 ))
+        ;;
+      # >= 4.2
       *)
-        PathToConvert="/${PathPrefixes[0]}$PathSuffix"
-        # add slash to the end of path in case of drive only path
-        (( ! ${#PathSuffix} )) && PathToConvert="$PathToConvert/"
-      ;;
+        printf -v PROJECT_LOG_FILE_NAME_SUFFIX "%(%Y'%m'%d_%H'%M'%S'')T$(( RANDOM % 1000 ))" -1
+        ;;
     esac
-  fi
+  }
 
-  RETURN_VALUE="$PathToConvert"
+  GetTime
 
-  return 0
+  PROJECT_LOG_DIR="$BASH_SOURCE_DIR/.log/$PROJECT_LOG_FILE_NAME_SUFFIX.${BASH_SOURCE_FILE_NAME%[.]*}"
+  PROJECT_LOG_FILE="$PROJECT_LOG_DIR/${PROJECT_LOG_FILE_NAME_SUFFIX}.${BASH_SOURCE_FILE_NAME%[.]*}.log"
+
+  [[ ! -e "$PROJECT_LOG_DIR" ]] && { mkdir -p "$PROJECT_LOG_DIR" || tkl_abort 11; }
+
+  # stdout+stderr redirection into the same log file with handles restore
+  {
+  {
+  {
+    exec $0 "$@" 2>&1 1>&8
+  } | tee -a "$PROJECT_LOG_FILE" 1>&9
+  } 8>&1 | tee -a "$PROJECT_LOG_FILE"
+  } 9>&2
+
+  exit $?
 }
-
-ScriptBaseInit "$@"
-
-ScriptFileNamePrefix="${ScriptFileName%[.]*}"
 
 function CallLr()
 {
@@ -130,68 +69,27 @@ function Call()
   return $LastError
 } 
 
-function GetTime()
-{
-  local separator=${1:-_}
-
-  date_prefix=$(date +%Y${separator}%m${separator}%H${separator}%M${separator}%S)
-  date_nanosec=$(date +%N)
-
-  date_formatted_msec=${date_nanosec}
-  while [[ "${date_formatted_msec:0:1}" == "0" ]]; do date_formatted_msec=${date_formatted_msec:1}; done
-  date_formatted_msec=$(( ${date_formatted_msec:-0} / 1000000 ))
-  printf -v date_formatted_msec "%03d" "$date_formatted_msec"
-}
-
 # WORKAROUND for `file is not found` when run from GUI shell
-cd "$ScriptDirPath"
+cd "$BASH_SOURCE_DIR"
 
-# with logs rotation
-LogFilesDir="$ScriptDirPath/logs"
-LogFileNamePrefix="$ScriptFileNamePrefix"
-LogFileName="$LogFileNamePrefix.log"
-LogFilePath="$LogFilesDir/$LogFileName"
-MaxFileSize=$(( 1024 * 1024 ))
+echo
+echo "----"
+echo "---- Start time: $PROJECT_LOG_FILE_NAME_SUFFIX"
+echo "---- CWD=\"$(pwd)\""
+echo "----"
 
-[[ ! -d "$LogFilesDir" ]] && mkdir "$LogFilesDir"
+# export variables
+Call export LD_LIBRARY_PATH=".:./lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
+#export QT_QPA_FONTDIR=./lib/fonts
 
-# print data and time of execution both to the log and into stdout
-(
-(
-(
-  GetTime
+CallLr "$BASH_SOURCE_DIR/${BASH_SOURCE_FILE_NAME%[.]*}" "$@"
 
-  if [[ -f "$LogFilePath" ]]; then
-    file_size=$(du -b "$LogFilePath" | { read -r size suffix; echo $size; })
-    if (( file_size > MaxFileSize )); then
-      timestamp="${date_prefix}_${date_formatted_msec}"
+GetTime
 
-      mv "$LogFilePath" "$LogFilesDir/$LogFileNamePrefix.$timestamp.log"
-      touch "$LogFilePath"
-    fi
-  fi
-
-  echo
-  echo "----"
-  echo "---- Start time: $date_prefix.$date_formatted_msec"
-  echo "---- cwd=\"$(pwd)\""
-  echo "----"
-
-  # export variables
-  Call export LD_LIBRARY_PATH=".:./lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
-  #export QT_QPA_FONTDIR=./lib/fonts
-
-  CallLr "$ScriptDirPath/$ScriptFileNamePrefix" "$@"
-
-  GetTime
-
-  echo "----"
-  echo "---- End time: $date_prefix.$date_formatted_msec"
-  echo "----"
-  echo
-
-) 2>&1 1>&5 | tee -a "$LogFilePath" >&9 2>&9 # temporary redirect stderr to the stream descriptor 9
-) 5>&1 | tee -a "$LogFilePath" >&8 2>&9 # temporary redirect stdout to the stream descriptor 8
-) 8>&1 9>&2 # redirect the stream descriptors 8 and 9 back to stdout and stderr
+echo "----"
+echo "---- End time: $PROJECT_LOG_FILE_NAME_SUFFIX"
+echo "---- Exit code: $LastError"
+echo "----"
+echo
 
 fi
