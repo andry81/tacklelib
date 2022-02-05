cfee7082b5ff97138977bd4bec1580d2db47cc58
andry81 <andry@inbox.ru>
2022-02-05 09:39:29 +0000

    2022.02.05: * fixed: CMakeLists.txt: missed to exclude `*.bak` files * new: include/tacklelib/utility/memory.hpp: `memcpy_bitwise64` function to bitwise copy on 64-bit padded buffers
    git-svn-id: svn+ssh://svn.code.sf.net/p/tacklelib/tacklelib/trunk@536 ffccfebd-399a-4e7c-9cd2-5fb927d1fecb

--- include/tacklelib/utility/memory.hpp
+++ include/tacklelib/utility/memory.hpp
@@ -241,6 +241,100 @@ namespace utility
         bool    m_is_reallocating;
     };
 
+    // Bitwise memory copy.
+    // Both buffers must be padded to 7 bytes remainder to be able to read/write the last 8-bit block as 64-bit block.
+    // Buffers must not overlap.
+    //
+    FORCE_INLINE void memcpy_bitwise64(uint8_t * to_padded_int64_buf, uint64_t to_first_bit_offset, uint8_t * from_padded_int64_buf, uint64_t from_first_bit_offset, uint64_t bit_size)
+    {
+        ASSERT_TRUE(bit_size);
+
+        uint64_t bit_offset = 0;
+
+        uint32_t from_byte_offset = uint32_t(from_first_bit_offset / 8);
+        uint32_t to_byte_offset = uint32_t(to_first_bit_offset / 8);
+
+        uint32_t remainder_from_bit_offset = uint32_t(from_first_bit_offset % 8);
+        uint32_t remainder_to_bit_offset = uint32_t(to_first_bit_offset % 8);
+
+        while (bit_offset < bit_size) {
+            if (remainder_to_bit_offset >= remainder_from_bit_offset && (remainder_to_bit_offset || remainder_from_bit_offset)) {
+                const uint64_t from_bit_block = *(uint64_t *)&from_padded_int64_buf[from_byte_offset];
+                uint64_t & to_bit_block = *(uint64_t *)&to_padded_int64_buf[to_byte_offset];
+
+                const uint32_t to_first_bit_delta_offset = remainder_to_bit_offset - remainder_from_bit_offset;
+                const uint64_t to_bit_block_inversed_mask = uint64_t(~0) << remainder_to_bit_offset;
+
+                to_bit_block = ((from_bit_block << to_first_bit_delta_offset) & to_bit_block_inversed_mask) | (to_bit_block & ~to_bit_block_inversed_mask);
+
+                const uint32_t bit_size_copied = 64 - remainder_to_bit_offset;
+
+                bit_offset += bit_size_copied;
+
+                from_first_bit_offset += bit_size_copied;
+                to_first_bit_offset += bit_size_copied;
+
+                if (remainder_to_bit_offset != remainder_from_bit_offset) {
+                    from_byte_offset += 7;
+                    to_byte_offset += 8;
+
+                    remainder_from_bit_offset = 8 - to_first_bit_delta_offset;
+                    remainder_to_bit_offset = 0;
+                }
+                else {
+                    from_byte_offset += 8;
+                    to_byte_offset += 8;
+
+                    remainder_from_bit_offset = 0;
+                    remainder_to_bit_offset = 0;
+                }
+            }
+            else if (remainder_to_bit_offset < remainder_from_bit_offset) {
+                const uint64_t from_bit_block = *(uint64_t *)&from_padded_int64_buf[from_byte_offset];
+                uint64_t & to_bit_block = *(uint64_t *)&to_padded_int64_buf[to_byte_offset];
+
+                const uint32_t to_first_bit_delta_offset = remainder_from_bit_offset - remainder_to_bit_offset;
+                const uint64_t to_bit_block_inversed_mask = uint64_t(~0) << remainder_to_bit_offset;
+
+                to_bit_block = ((from_bit_block >> to_first_bit_delta_offset) & to_bit_block_inversed_mask) | (to_bit_block & ~to_bit_block_inversed_mask);
+
+                const uint32_t bit_size_copied = 64 - remainder_from_bit_offset;
+
+                bit_offset += bit_size_copied;
+
+                from_first_bit_offset += bit_size_copied;
+                to_first_bit_offset += bit_size_copied;
+
+                from_byte_offset += 8;
+                to_byte_offset += 7;
+
+                remainder_from_bit_offset = 0;
+                remainder_to_bit_offset = (8 - to_first_bit_delta_offset);
+            }
+            // optimization
+            else {
+                const uint64_t bit_size_remain = bit_size - bit_offset;
+                const uint32_t byte_size_remain = uint32_t(bit_size_remain / 8);
+
+                if (byte_size_remain + 1 > 8) {
+                    memcpy(to_padded_int64_buf + to_byte_offset, from_padded_int64_buf + from_byte_offset, byte_size_remain + 1);
+                }
+                // optimization
+                else {
+                    *(uint64_t *)to_padded_int64_buf[to_byte_offset] = *(uint64_t *)from_padded_int64_buf[from_byte_offset];
+                }
+
+                break;
+            }
+
+            ASSERT_EQ(from_byte_offset, uint32_t(from_first_bit_offset / 8));
+            ASSERT_EQ(remainder_from_bit_offset, uint32_t(from_first_bit_offset % 8));
+
+            ASSERT_EQ(to_byte_offset, uint32_t(to_first_bit_offset / 8));
+            ASSERT_EQ(remainder_to_bit_offset, uint32_t(to_first_bit_offset % 8));
+        }
+    }
+
 }
 
 #endif
