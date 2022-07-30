0493216e8b9cad79607164f1eb8d3ce3d8a84286
andry81 <andry@inbox.ru>
2022-07-30 15:59:56 +0000

    2022.07.30: * new: python/tacklelib: added `__init__.py` for initialization support as a module
    git-svn-id: svn+ssh://svn.code.sf.net/p/tacklelib/tacklelib/trunk@560 ffccfebd-399a-4e7c-9cd2-5fb927d1fecb

---
+++ python/tacklelib/__init__/__init__.py
@@ -0,0 +1,15 @@
+
+import os, sys, inspect
+
+SOURCE_FILE = os.path.normcase(os.path.abspath(inspect.getsourcefile(lambda:0))).replace('\\','/')
+SOURCE_DIR = os.path.dirname(SOURCE_FILE)
+
+# portable import to the global space
+sys.path.append(os.path.abspath(SOURCE_DIR + '/..').replace('\\','/'))
+import tacklelib as tkl
+
+tkl.tkl_init(tkl)
+
+# cleanup
+del tkl # must be instead of `tkl = None`, otherwise the variable would be still persist
+sys.path.pop()
