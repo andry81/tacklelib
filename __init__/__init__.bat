83c919d07d6ce839c720c1ed6c9bb5635642f55a
andry81 <andry@inbox.ru>
2022-10-15 04:42:30 +0000

    2022.10.15: * new: __init__: added `NO_GEN` and `NO_LOG` environment variables to disable files and directories generation and logging
    git-svn-id: svn+ssh://svn.code.sf.net/p/tacklelib/tacklelib/trunk@566 ffccfebd-399a-4e7c-9cd2-5fb927d1fecb

--- __init__/__init__.bat
+++ __init__/__init__.bat
@@ -21,14 +21,23 @@ if exist "%TACKLELIB_PROJECT_EXTERNALS_ROOT%/contools/__init__/__init__.bat" (
   call "%%TACKLELIB_PROJECT_EXTERNALS_ROOT%%/contools/__init__/__init__.bat" %%* || exit /b
 )
 
-if not exist "%TACKLELIB_PROJECT_OUTPUT_CONFIG_ROOT%\" ( mkdir "%TACKLELIB_PROJECT_OUTPUT_CONFIG_ROOT%" || exit /b 10 )
+if %NO_GEN%0 EQU 0 (
+  call "%%CONTOOLS_ROOT%%/std/mkdir_if_notexist.bat" "%%TACKLELIB_PROJECT_OUTPUT_CONFIG_ROOT%%" || exit /b 10
+)
 
 if not defined LOAD_CONFIG_VERBOSE if %INIT_VERBOSE%0 NEQ 0 set LOAD_CONFIG_VERBOSE=1
 
-call "%%CONTOOLS_ROOT%%/build/load_config_dir.bat" %%* -lite_parse -gen_user_config "%%TACKLELIB_PROJECT_INPUT_CONFIG_ROOT%%" "%%TACKLELIB_PROJECT_OUTPUT_CONFIG_ROOT%%" || exit /b
+if %NO_GEN%0 EQU 0 (
+  call "%%CONTOOLS_ROOT%%/build/load_config_dir.bat" %%* -lite_parse -gen_user_config "%%TACKLELIB_PROJECT_INPUT_CONFIG_ROOT%%" "%%TACKLELIB_PROJECT_OUTPUT_CONFIG_ROOT%%" || exit /b
+) else call "%%CONTOOLS_ROOT%%/build/load_config_dir.bat" %%* -lite_parse "%%TACKLELIB_PROJECT_INPUT_CONFIG_ROOT%%" "%%TACKLELIB_PROJECT_OUTPUT_CONFIG_ROOT%%" || exit /b
 
-if not exist "%PROJECT_OUTPUT_ROOT%\" ( mkdir "%PROJECT_OUTPUT_ROOT%" || exit /b 11 )
-if not exist "%PROJECT_LOG_ROOT%\" ( mkdir "%PROJECT_LOG_ROOT%" || exit /b 12 )
+if %NO_GEN%0 EQU 0 (
+  call "%%CONTOOLS_ROOT%%/std/mkdir_if_notexist.bat" "%%PROJECT_OUTPUT_ROOT%%" || exit /b 11
+
+  if %NO_LOG%0 EQU 0 (
+    call "%%CONTOOLS_ROOT%%/std/mkdir_if_notexist.bat" "%%PROJECT_LOG_ROOT%%" || exit /b 12
+  )
+)
 
 if defined CHCP call "%%CONTOOLS_ROOT%%/std/chcp.bat" %%CHCP%%
 
