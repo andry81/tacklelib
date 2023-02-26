6b8c2b8d62e05f6bdde9c7e159c4531e49663454
andry81 <andry@inbox.ru>
2023-02-26 12:52:44 +0300

    2023.02.26: * new: bash/tacklelib/tools/git/git_filter_branch_cleanup.sh: added `git_filter_branch_cleanup.sh` script to cleanup artefacts after branch filtering with the `git_filter_branch_user.sh` script execution

---
+++ bash/tacklelib/tools/git/git_filter_branch_cleanup.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Description:
+#   Script to cleanup artefacts after branch filtering with the
+#   `git_filter_branch_user.sh` script execution.
+
+# Usage:
+#   git_filter_branch_cleanup.sh
+#
+
+# Script both for execution and inclusion.
+if [[ -n "$BASH" ]]; then
+
+function call()
+{
+  local IFS=$' \t'
+  echo ">$*"
+  "$@"
+}
+
+function git_filter_branch_cleanup()
+{
+  # print all refs
+  call git show-ref || exit 255
+  echo ---
+
+  local IFS
+
+  local hash
+  local ref
+
+  # remove all original refs
+  IFS=$' \t'; while read -r hash ref; do
+    [[ "${ref:0:19}" != "refs/original/refs/" ]] && continue
+    ref_remote="${ref:19}"
+    [[ -z "$ref_remote" ]] && continue
+    call git update-ref -d "$ref"
+  done <<< `git show-ref 2>/dev/null`
+
+  return 0
+}
+
+# shortcut
+function git_fb_cl()
+{
+  git_filter_branch_cleanup "$@"
+}
+
+if [[ -z "$BASH_LINENO" || BASH_LINENO[0] -eq 0 ]]; then
+  # Script was not included, then execute it.
+  git_filter_branch_cleanup "$@"
+fi
+
+fi
