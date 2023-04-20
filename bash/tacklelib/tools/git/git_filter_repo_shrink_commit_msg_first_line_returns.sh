0a55cf23d2fe28758b8dc344571a720cf2d118ad
andry81 <andry@inbox.ru>
2023-04-20 13:47:14 +0300

    2023.04.20: * changed: bash/tacklelib/tools/git/git_filter_repo_shrink_commit_msg_first_line_returns.sh: imply `--partial` flag by default to avoid remove the origin remote

--- bash/tacklelib/tools/git/git_filter_repo_shrink_commit_msg_first_line_returns.sh
+++ bash/tacklelib/tools/git/git_filter_repo_shrink_commit_msg_first_line_returns.sh
@@ -18,6 +18,25 @@
 #   git_filter_repo_shrink_commit_msg_first_line_returns.sh --force
 #
 
+# NOTE:
+#   The implementation implies the `--partial` flag to avoid remove of the
+#   origin remote.
+#
+#   See details in the documentation:
+#
+#     https://htmlpreview.github.io/?https://github.com/newren/git-filter-repo/blob/docs/html/git-filter-repo.html#INTERNALS
+#
+#   --partial
+#
+#     Do a partial history rewrite, resulting in the mixture of old and new
+#     history. This implies a default of update-no-add for --replace-refs,
+#     disables rewriting refs/remotes/origin/* to refs/heads/*, disables
+#     removing of the origin remote, disables removing unexported refs,
+#     disables expiring the reflog, and disables the automatic post-filter gc.
+#     Also, this modifies --tag-rename and --refname-callback options such that
+#     instead of replacing old refs with new refnames, it will instead create
+#     new refs and keep the old ones around. Use with caution.
+
 # Script both for execution and inclusion.
 if [[ -n "$BASH" ]]; then
 
@@ -35,7 +54,7 @@ function git_filter_repo_shrink_commit_msg_first_line_returns()
 msg = commit.message.decode("utf-8")
 msg_new = re.sub(r'\''[\r\n]*([^\r\n]+)(\r?\n)[\r\n]*?(.*)'\'', r'\''\1\2\3'\'', msg)
 commit.message = msg_new.encode("utf-8")
-' "$@"
+' --partial "$@"
 
   return 0
 }
