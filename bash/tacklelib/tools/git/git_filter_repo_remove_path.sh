c7a972fa89ba51d13a8a5da4b7e300cc8a3921b3
andry81 <andry@inbox.ru>
2023-05-24 13:40:43 +0300

    2023.05.24: * changed: bash/tacklelib/tools/git/git_filter_repo_remove_path*.sh: added usage of `--partial` flag by default * changed: bash/tacklelib/tools/git/git_filter_repo_remove_path*.sh: more details

--- bash/tacklelib/tools/git/git_filter_repo_remove_path.sh
+++ bash/tacklelib/tools/git/git_filter_repo_remove_path.sh
@@ -17,13 +17,55 @@
 # Examples:
 #   >
 #   cd myrepo/path
-#   git_filter_repo_remove_path.sh _externals/ --force
+#   git_filter_repo_remove_path.sh dir1/ --refs dev ^t1 ^master --force
 #
-#   CAUTION:
-#     In example above all paths has contained `_externals` will be removed:
-#     * `_externals/`
-#     * `_externals/dir1`
-#     * `_externals/file1`
+#   NOTE:
+#     * `dir1`            - (dir) removed
+#     * `dir1/dir2`       - (dir) removed
+#     * `dir1/dir2/file1` - (file) removed
+#     * `dir2/dir1`       - (dir) NOT removed
+#
+#   >
+#   cd myrepo/path
+#   git_filter_repo_remove_path.sh file1 --refs dev ^t1 ^master --force
+#
+#   NOTE:
+#     * `file1`           - (file) removed
+#     * `dir1/file1`      - (file) NOT removed
+#
+#   >
+#   cd myrepo/path
+#   git_filter_repo_remove_path.sh file2/ --refs dev ^t1 ^master --force
+#
+#   NOTE:
+#     * `file2`           - (file) NOT removed
+#
+#   >
+#   cd myrepo/path
+#   git_filter_repo_remove_path.sh dir-or-file --refs dev ^t1 ^master --force
+#
+#   NOTE:
+#     * `dir-or-file`     - (file/dir) removed
+#
+
+# NOTE:
+#   The implementation implies the `--partial` flag to avoid remove of the
+#   origin remote.
+#
+#   See details in the documentation:
+#
+#     https://htmlpreview.github.io/?https://github.com/newren/git-filter-repo/blob/docs/html/git-filter-repo.html#INTERNALS
+#
+#   --partial
+#
+#     Do a partial history rewrite, resulting in the mixture of old and new
+#     history. This implies a default of update-no-add for --replace-refs,
+#     disables rewriting refs/remotes/origin/* to refs/heads/*, disables
+#     removing of the origin remote, disables removing unexported refs,
+#     disables expiring the reflog, and disables the automatic post-filter gc.
+#     Also, this modifies --tag-rename and --refname-callback options such that
+#     instead of replacing old refs with new refnames, it will instead create
+#     new refs and keep the old ones around. Use with caution.
 
 # NOTE:
 #   You must use `git_filter_repo_cleanup.sh` script to complete the
@@ -43,7 +85,7 @@ function git_filter_repo_remove_path()
 {
   local path="$1"
 
-  call git filter-repo --invert-paths --path "$path" "${@:2}"
+  call git filter-repo --partial --invert-paths --path "$path" "${@:2}"
 }
 
 # shortcut
--- bash/tacklelib/tools/git/git_filter_repo_remove_path_list.sh
+++ bash/tacklelib/tools/git/git_filter_repo_remove_path_list.sh
@@ -19,16 +19,36 @@
 # Examples:
 #   >
 #   cd myrepo/path
-#   git_filter_repo_remove_path_list.sh mydir/ myfile.txt // --force
+#   git_filter_repo_remove_path_list.sh dir1/ file1 file2/ dir-or-file // --refs dev ^t1 ^master --force
 #
-#   CAUTION:
-#     In example above all paths has contained `mydir` and `myfile` will be
-#     removed:
-#     * `mydir/`
-#     * `mydir/dir1`
-#     * `mydir/dir1/file1`
-#     * `myfile`
-#     * `dir1/myfile`
+#   NOTE:
+#     * `dir1`            - (dir) removed
+#     * `dir1/dir2`       - (dir) removed
+#     * `dir1/dir2/file1` - (file) removed
+#     * `dir2/dir1`       - (dir) NOT removed
+#     * `file1`           - (file) removed
+#     * `dir2/file1`      - (file) NOT removed
+#     * `file2`           - (file) NOT removed
+#     * `dir-or-file`     - (file/dir) removed
+
+# NOTE:
+#   The implementation implies the `--partial` flag to avoid remove of the
+#   origin remote.
+#
+#   See details in the documentation:
+#
+#     https://htmlpreview.github.io/?https://github.com/newren/git-filter-repo/blob/docs/html/git-filter-repo.html#INTERNALS
+#
+#   --partial
+#
+#     Do a partial history rewrite, resulting in the mixture of old and new
+#     history. This implies a default of update-no-add for --replace-refs,
+#     disables rewriting refs/remotes/origin/* to refs/heads/*, disables
+#     removing of the origin remote, disables removing unexported refs,
+#     disables expiring the reflog, and disables the automatic post-filter gc.
+#     Also, this modifies --tag-rename and --refname-callback options such that
+#     instead of replacing old refs with new refnames, it will instead create
+#     new refs and keep the old ones around. Use with caution.
 
 # NOTE:
 #   You must use `git_filter_repo_cleanup.sh` script to complete the
@@ -69,7 +89,7 @@ function git_filter_repo_remove_path_list()
     return 255
   fi
 
-  evalcall0 "git filter-repo --invert-paths$path_list_cmdline" "$@"
+  evalcall0 "git filter-repo --partial --invert-paths$path_list_cmdline" "$@"
 }
 
 # shortcut
