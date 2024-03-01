f94e452d8db1e37f93f0912a4aa1bdab7a03e271
andry81 <andry@inbox.ru>
2024-03-01 21:42:17 +0300

    2024.03.01: * changed: _config: switched all configs to use uniform config files loader * changed: __init__: switched init script to use uniform config files loader

--- __init__/__init__.bat
+++ __init__/__init__.bat
@@ -49,8 +49,8 @@ if %NO_GEN%0 EQU 0 (
 if not defined LOAD_CONFIG_VERBOSE if %INIT_VERBOSE%0 NEQ 0 set LOAD_CONFIG_VERBOSE=1
 
 if %NO_GEN%0 EQU 0 (
-  call "%%CONTOOLS_ROOT%%/build/load_config_dir.bat" %%* -lite_parse -gen_user_config "%%TACKLELIB_PROJECT_INPUT_CONFIG_ROOT%%" "%%TACKLELIB_PROJECT_OUTPUT_CONFIG_ROOT%%" || exit /b
-) else call "%%CONTOOLS_ROOT%%/build/load_config_dir.bat" %%* -lite_parse "%%TACKLELIB_PROJECT_INPUT_CONFIG_ROOT%%" "%%TACKLELIB_PROJECT_OUTPUT_CONFIG_ROOT%%" || exit /b
+  call "%%CONTOOLS_ROOT%%/build/load_config_dir.bat" %%* -gen_user_config "%%TACKLELIB_PROJECT_INPUT_CONFIG_ROOT%%" "%%TACKLELIB_PROJECT_OUTPUT_CONFIG_ROOT%%" || exit /b
+) else call "%%CONTOOLS_ROOT%%/build/load_config_dir.bat" %%* "%%TACKLELIB_PROJECT_INPUT_CONFIG_ROOT%%" "%%TACKLELIB_PROJECT_OUTPUT_CONFIG_ROOT%%" || exit /b
 
 if %NO_GEN%0 EQU 0 (
   call "%%CONTOOLS_ROOT%%/std/mkdir_if_notexist.bat" "%%PROJECT_OUTPUT_ROOT%%" || exit /b 11
