2ac17eb7406ff4a13ea683a7a88c710260a05e5b
andry81 <andry@inbox.ru>
2025-07-18 17:16:34 +0300

    2025.07.18: * changed: __init__.sh: avoid parameters pass to the root init script from a sub directory (if you want to pass a script parameter, then you have to pass it in to a script from the same directory or below it)

--- bash_tests/__init__/__init__.sh
+++ bash_tests/__init__/__init__.sh
@@ -3,7 +3,7 @@
 # Script can be ONLY included by "source" command.
 [[ -n "$BASH" && (-z "$BASH_LINENO" || BASH_LINENO[0] -gt 0) && (-z "$TACKLELIB_BASH_TESTS_PROJECT_ROOT_INIT0_DIR" || "$TACKLELIB_BASH_TESTS_PROJECT_ROOT_INIT0_DIR" != "$TACKLELIB_BASH_TESTS_PROJECT_ROOT") ]] || return 0 || exit 0 # exit to avoid continue if the return can not be called
 
-tkl_include_or_abort "../../__init__/__init__.sh" "${@:2}"
+tkl_include_or_abort "../../__init__/__init__.sh"
 
 [[ -n "$TACKLELIB_BASH_TESTS_PROJECT_ROOT" ]] ||                tkl_export_path -a -s TACKLELIB_BASH_TESTS_PROJECT_ROOT                 "$BASH_SOURCE_DIR/.."
 [[ -n "$TACKLELIB_BASH_TESTS_PROJECT_LOG_ROOT" ]] ||            tkl_export_path -a -s TACKLELIB_BASH_TESTS_PROJECT_LOG_ROOT             "$TACKLELIB_BASH_TESTS_PROJECT_ROOT/.log" # particular project variable
